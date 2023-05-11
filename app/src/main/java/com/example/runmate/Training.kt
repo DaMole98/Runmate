e7013e04538a273492b1d8afcf8eef49e6eac4d6
MDBuckaroo <mattiadangelo756@gmail.com>
2023-05-11 18:04:06 +0200

    aggiornamento test Training.kt

--- app/src/main/java/com/example/runmate/Training.kt
+++ app/src/main/java/com/example/runmate/Training.kt
@@ -18,7 +18,7 @@ class Training : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_training)
-
+//ciaone
         val btn = findViewById<ImageButton>(R.id.trainingbutton)
         btn.setImageResource(R.drawable.allenamento_black)
 
