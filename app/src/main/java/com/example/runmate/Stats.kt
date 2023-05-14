f2283030a501b440b7450deab66eabbaea69bc15
MDBuckaroo <mattiadangelo756@gmail.com>
2023-05-14 20:43:01 +0200

    Aggiunto grafica stats + modificato e aggiunto grafica training + trasferito contapassi da activity a fragment

--- app/src/main/java/com/example/runmate/Stats.kt
+++ app/src/main/java/com/example/runmate/Stats.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.ProgressBar
+import android.widget.TextView
 
 class Stats : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/runmate/Training.kt
+++ app/src/main/java/com/example/runmate/Training.kt
@@ -5,16 +5,11 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
 import android.widget.TextView
-import kotlin.math.roundToInt
 
 class Training : Activity() {
-    private lateinit var tv_totalSteps: TextView
-    private lateinit var tv_totalDistance: TextView
-    private lateinit var tv_totalCalories: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_training)
@@ -39,30 +34,5 @@ class Training : Activity() {
             val intent = Intent(this, Settings::class.java)
             startActivity(intent)
         }*/
-
-        ////// DA QUI CODICE PER CONTAPASSI (ASSIEME A CaloriesService)
-
-        registerReceiver(updateUIReceiver, IntentFilter("UPDATE_UI"))
-
-        // Start the service
-        val serviceIntent = Intent(this, CaloriesService::class.java)
-        startService(serviceIntent)
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-
-        unregisterReceiver(updateUIReceiver)
-        sendBroadcast(Intent("STOP_SERVICE"))
-    }
-
-    private val updateUIReceiver = object : BroadcastReceiver() {
-        override fun onReceive(context: Context?, intent: Intent?) {
-            tv_totalSteps.text = intent?.getIntExtra("totalSteps", 0).toString()
-            tv_totalDistance.text = "${intent?.getIntExtra("totalDistance", 0)} m"
-
-            // TODO(tv_totalCalories.text = "${intent?.getFloatExtra("totalCalories", 0f)?.roundToInt()} kcal")
-            tv_totalCalories.text = String.format("%.${3}f kcal", intent?.getFloatExtra("totalCalories", 0f))
-        }
     }
 }
