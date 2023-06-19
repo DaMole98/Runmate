d72f838cae5a56f2d41360d7e972df8f9529e64c
VVLGPP <vivaldigiuseppe99@gmail.com>
2023-06-19 19:33:15 +0200

    Sistemato bug back button

--- app/src/main/java/com/example/runmate/Registration.kt
+++ app/src/main/java/com/example/runmate/Registration.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
